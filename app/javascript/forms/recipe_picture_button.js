3098f7c3413eec086c38e3d42db01046e331d0b4
dornby <hugo@getaround.com>
2020-11-19 13:50:08 +0100

    Fix JS fail when not on the right page

--- app/javascript/forms/recipe_picture_button.js
+++ app/javascript/forms/recipe_picture_button.js
@@ -2,8 +2,10 @@ window.addEventListener('load', () => {
   const actualBtn = document.getElementById('recipe_picture');
   const newButton = document.getElementById('recipe-picture-button');
 
+  if (actualBtn) {
     actualBtn.addEventListener('change', function(){
       newButton.textContent = this.files[0].name
       newButton.classList.add("file-chosen")
     })
+  }
 })
