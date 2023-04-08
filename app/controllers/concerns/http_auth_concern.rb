65f76c603772599dfb83ce5c7125dac14500b5f1
dornby <hugo@getaround.com>
2023-04-08 12:25:25 +0200

    Test another IP based on logs

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -10,7 +10,7 @@ module HttpAuthConcern # rubocop:disable Style/Documentation
   def http_authenticate
     return true unless Rails.env == "production"
     return true if request.remote_ip == "78.193.208.180"
-    return true if request.remote_ip == "88.165.91.118"
+    return true if request.remote_ip == "2a09:8280:1::6:68e3"
 
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["HTTP_BASIC_AUTH_USERNAME"] &&
